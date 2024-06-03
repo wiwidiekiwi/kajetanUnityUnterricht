fd6dac58948cf57dad540c7e4f10bbde2cacc305
wiwi <stanislawskiwojciech47@gmail.com>
2024-06-03 14:27:41 +0200

    movement started

--- Assets/Scenes/Scripts/CharacterControlling/CharacterMovement.cs
+++ Assets/Scenes/Scripts/CharacterControlling/CharacterMovement.cs
@@ -5,15 +5,23 @@ using UnityEngine.InputSystem;
 
 public class CharacterMovement : MonoBehaviour
 {
-    private int counter = 0;
-    private int JumpCounter = 0;
-    private int MoveCounter = 0;
-    private int SprintCounter = 0;
-    private int SneakCounter = 0;
+    //private int counter = 0;
+   // private int JumpCounter = 0;
+   // private int MoveCounter = 0;
+   // private int SprintCounter = 0;
+   // private int SneakCounter = 0;
+
+   private Rigidbody2D rb;
+   private float inputDirection;
+
+   // [] with alt gr + 8 & 9
+   [SerializeField] private float movementSpeed = 10f;
+   [SerializeField] private float jumpForce = 10f;
     
     // Start is called before the first frame update
     void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
         Debug.Log("Start!");
     }
 
@@ -24,27 +32,38 @@ public class CharacterMovement : MonoBehaviour
        // Debug.Log("Update! Framenumber: " + counter);
     }
 
+    //physics based things go in there
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(inputDirection * movementSpeed, rb.velocity.y);
+    }
     void OnJump()
     {
-        JumpCounter = JumpCounter + 1;
-        Debug.Log("Jump! " + JumpCounter);
+        rb.velocity = new Vector2(0f, jumpForce);
+       // JumpCounter = JumpCounter + 1;
+        Debug.Log("Jump! ");
     }
 
-    void OnMove()
+    void OnMove(InputValue inputValue)
     {
-        MoveCounter = MoveCounter + 1;
-        Debug.Log("Move! " + MoveCounter);
+       // MoveCounter = MoveCounter + 1;
+        inputDirection = inputValue.Get<float>();
+        Debug.Log("Move! " + inputDirection);
     }
 
-    void OnSprint()
+    void OnSprint(InputValue inputValue)
     {
-        SprintCounter = SprintCounter + 1;
-        Debug.Log("Sprint! " + SprintCounter);
+       // SprintCounter = SprintCounter + 1;
+       float isSprinting = inputValue.Get<float>();
+        Debug.Log("Sprint! " + isSprinting);
     }
 
-    void OnSneak()
+    void OnSneak(InputValue inputValue)
     {
-        SneakCounter = SneakCounter + 1;
-        Debug.Log("Sneak! " + SneakCounter);
+        //SneakCounter = SneakCounter + 1;
+        float isSneaking = inputValue.Get<float>();
+        Debug.Log("Sneak! " + isSneaking);
     }
+    
+    
 }
